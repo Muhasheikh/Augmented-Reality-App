e394cbc44c0457997ae8356218b055e859253881
Muhasheikh <muhasheikh8@gmail.com>
2022-04-06 21:04:29 +0530

    Intergrated Final Application

---
+++ app.js
@@ -0,0 +1,7 @@
+
+      document.getElementById("marker").addEventListener("markerFound", (e) => {
+        document.getElementById("marker-image").style.display = "none";
+      });
+
+      
+      
